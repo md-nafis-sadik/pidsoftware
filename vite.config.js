0f67d0dc200257b28776cc375921b4d439cebe7e
md-nafis-sadik <sadikmdnafis@gmail.com>
2024-08-10 20:16:09 +0600

    another

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/pidsoftware/',  // Change this to the name of your GitHub repo
+  base: '/pidsoftware/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx', // Make sure this points to your main entry file
+      },
+    },
+  },
 });
